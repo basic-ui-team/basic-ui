6bb1c697d3edb90404f98831e4985ab22b8098be
Leonard Atorough <leotorough@gmail.com>
2026-05-16 16:47:18 +0100

    feat: implement provider builder and associated utility functions for testing

---
+++ packages/core/src/test-utils/index.ts
@@ -0,0 +1,6 @@
+export * from "./renderWithProviders";
+export * from "./setupUser";
+export * from "./renderHookWithProviders";
+export * from "./createProviderBuilder";
+
+export { screen, within, waitFor, fireEvent } from "@testing-library/react";
