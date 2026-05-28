82dd15425d3efca7ba626524d1c902c33750cb20
Leonard Atorough <leotorough@gmail.com>
2026-05-28 14:37:22 +0100

    feat: add type-check script to package.json files across multiple packages

--- packages/core/src/components/Card/Unstyled/Unstyled.test.tsx
+++ packages/core/src/components/Card/Unstyled/Unstyled.test.tsx
@@ -1,5 +1,4 @@
 import { describe, it, expect, vi } from "vitest";
-import React from "react";
 import { CardUnstyled } from "./Unstyled";
 import { renderWithProviders, screen, setupUser, fireEvent } from "../../../test-utils";
 
